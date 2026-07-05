2f85fd9f1d6c843d3ae6bedf3b83e30a36f697e3
Ahmed Fouad Abodeaf <100656655+ahmed7-fouad@users.noreply.github.com>
2026-07-06 02:49:10 +0300

    Refactor code structure for improved readability and maintainability

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
+import Nav from "./components/shared/Nav"
+import ChooseSpecialistPage from "./features/chooseSpecialist/components/ChooseSpecialistPage";
 const App = () => {
     return (
-        <></>
-    )
+      <>
+        <Nav />
+        <ChooseSpecialistPage/>
+      </>
+    );
 }
 
 export default App
